b30ee885366f3a410776630209a30d761e7b6209
Agustin Kassis <webmaster@masize.com>
2025-10-16 10:10:57 -0300

    add InitializeCardResponse type

--- src/types/response.ts
+++ src/types/response.ts
@@ -4,3 +4,12 @@ export type LoginResponse = {
   skins: Skin[];
   lnurlwBase: string;
 };
+
+export type InitializeCardResponse = {
+  k0: string;
+  k1: string;
+  k2: string;
+  k3: string;
+  k4: string;
+  privateUID: string;
+};
